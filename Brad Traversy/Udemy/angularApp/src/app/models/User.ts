d47d826d883d48473c6c57129bfbfdf1fce8f8d6
Gourimath, Sagar <sagar.gourimath@gmail.com>
2021-09-30 18:45:01 +0530

    use user component.

--- Brad Traversy/Udemy/angularApp/src/app/models/User.ts
+++ Brad Traversy/Udemy/angularApp/src/app/models/User.ts
@@ -1,11 +1,11 @@
 export interface User {
   firstName: string;
   lastName: string;
-  age: 30;
-  address: {
-    street: string;
-    city: string;
-    state: string;
+  age?: 30;
+  address?: {
+    street?: string;
+    city?: string;
+    state?: string;
   };
   isActive?: boolean;
   registered?: any;
